fc80bf8f17d6c310c168ca439c858c0ac04fd28a
Athman Gude <athmangude@gmail.com>
2016-03-29 11:32:52 +0300

    materialised the app

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -8,19 +8,23 @@ import MessageList from 'components/message-list';
 import MessageEntryBox from 'components/message-entry-box';
 import * as messageActionCreators from 'actions/message-actions';
 
-import {AppBar} from 'material-ui';
+import {AppBar, Card} from 'material-ui';
+import MuiThemeProvider from 'material-ui/lib/MuiThemeProvider';
+import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <AppBar />
-                <MessageList userId={this.props.userId} messages={this.props.message} />
-                <MessageEntryBox
+                <Card>
+                    <AppBar title="Awesome Chat Window" />
+                    <MessageList userId={this.props.userId} messages={this.props.message} />
+                    <MessageEntryBox
                     value={this.props.currentMessage}
                     userId={this.props.userId}
                     onChange={this.props.updateMessage}
                     onSubmit={this.props.addMessage} />
+                </Card>
             </div>
         );
     }
@@ -39,3 +43,4 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default MuiThemeProvider(getMuiTheme(null, { userAgent: 'all' }))();
